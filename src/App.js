7c16b0942e7ee0887764ca7c0f225d239dfa0de0
GiftSibiya <giftsibiya.gb@gmail.com>
2024-02-21 19:27:51 +0200

    second build deploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>This is the Second build deploy test</p>
+        <p>This right here is the Second build deploy test</p>
       </header>
     </div>
   );
